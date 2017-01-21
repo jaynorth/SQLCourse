8eb227379bdad7fa352b6bb214516b6a8890cc31
jay <jayrdee@gmail.com>
2017-01-21 14:08:32 +0100

    2nd commit

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Testing Git");
+            Console.WriteLine("Testing Git 2nd commit");
             Console.ReadKey();
         }
     }
