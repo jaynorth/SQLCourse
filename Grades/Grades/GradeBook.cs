db0d8bcdc4e0b04f5227748a46051c5c2cd3f8f0
jay <jayrdee@gmail.com>
2017-01-22 16:53:03 +0100

    modif finished

---
+++ Grades/Grades/GradeBook.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class GradeBook
+    {
+        public GradeBook()
+        {
+            grades = new List<float>();
+        }
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        public GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats =  new GradeStatistics();
+
+            //Calculate Average
+            float sum = 0f;
+     
+            foreach (float grade in grades) {
+                sum += grade;
+                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+                stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+            }
+
+            stats.AverageGrade = sum / grades.Count;
+
+
+
+            return stats;
+        }
+
+        List<float> grades ;
+
+
+    }
+}
---
+++ Grades/Grades/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            GradeBook book = new GradeBook();
+            book.AddGrade(91.7f);
+            book.AddGrade(67.7f);
+            book.AddGrade(76.8f);
+
+            GradeStatistics stats = book.ComputeStatistics();
+            Console.WriteLine(stats.AverageGrade);
+            Console.WriteLine(stats.HighestGrade);
+            Console.WriteLine(stats.LowestGrade);
+
+            Console.ReadKey();
+
+        }
+    }
+}
