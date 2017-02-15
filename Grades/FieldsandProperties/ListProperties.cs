d4ad8e2454db9967c2fca42cfdf8242e338b66b2
jayweb <jayweb@JAYWEB-PC>
2017-02-15 15:06:59 +0100

    List property added

---
+++ Grades/FieldsandProperties/ListProperties.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FieldsandProperties
+{
+    class ListProperties
+    {
+
+        public ListProperties()
+        {
+            this.NumberList = new List<int>();
+        }
+
+
+        private List<int> _numberList;
+
+        public List<int> NumberList
+        {
+            get { return _numberList; }
+            private set { _numberList = value; }
+        }
+
+
+
+    }
+}
--- Grades/FieldsandProperties/Program.cs
+++ Grades/FieldsandProperties/Program.cs
@@ -20,28 +20,39 @@ namespace FieldsandProperties
             //Console.WriteLine(v.Name);
             //Console.ReadKey();
 
-            Dice dice1 = new Dice();
-            Dice dice2 = new Dice();
-            Dice dice3 = new Dice();
-            Dice dice4 = new Dice();
-            Dice dice5 = new Dice();
-            Dice dice6 = new Dice();
+            //Dice dice1 = new Dice();
+            //Dice dice2 = new Dice();
+            //Dice dice3 = new Dice();
+            //Dice dice4 = new Dice();
+            //Dice dice5 = new Dice();
+            //Dice dice6 = new Dice();
+            //
+            //Console.WriteLine("Number of Dice Objects is " + Dice.NumberObjects());
+            //
+            //
+            //dice1.Throwdice();
+            //dice2.Throwdice();
+            //dice3.Throwdice();
+            //dice4.Throwdice();
+            //dice5.Throwdice();
+            //dice6.Throwdice();
+            //Console.WriteLine("dice1 number: " + dice1.DiceNumber);
+            //Console.WriteLine("dice2 number: " + dice2.DiceNumber);
+            //Console.WriteLine("dice3 number: " + dice3.DiceNumber);
+            //Console.WriteLine("dice4 number: " + dice4.DiceNumber);
+            //Console.WriteLine("dice5 number: " + dice5.DiceNumber);
+            //Console.WriteLine("dice6 number: " + dice6.DiceNumber);
 
-            Console.WriteLine("Number of Dice Objects is " + Dice.NumberObjects());
+            ListProperties l = new ListProperties();
 
-            
-            dice1.Throwdice();
-            dice2.Throwdice();
-            dice3.Throwdice();
-            dice4.Throwdice();
-            dice5.Throwdice();
-            dice6.Throwdice();
-            Console.WriteLine("dice1 number: " + dice1.DiceNumber);
-            Console.WriteLine("dice2 number: " + dice2.DiceNumber);
-            Console.WriteLine("dice3 number: " + dice3.DiceNumber);
-            Console.WriteLine("dice4 number: " + dice4.DiceNumber);
-            Console.WriteLine("dice5 number: " + dice5.DiceNumber);
-            Console.WriteLine("dice6 number: " + dice6.DiceNumber);
+
+            l.NumberList.Add(76);
+            l.NumberList.Add(87);
+
+            foreach (int i in l.NumberList) { 
+
+            Console.WriteLine(i);
+            }
 
             Console.ReadKey();
 
