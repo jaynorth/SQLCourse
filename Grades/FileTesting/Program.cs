e5cadad400abb6453c00e32bef15c875427c21d7
jayweb <jayweb@JAYWEB-PC>
2017-02-16 15:47:46 +0100

    added reading with streamreader

---
+++ Grades/FileTesting/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            
+
+            string filePath = @"nameList.txt";
+            StreamReader sr = new StreamReader(filePath); //Create StreamReader object (using System.IO)
+            string line = sr.ReadLine();
+            while (line != null)
+            {
+                string[] tab = line.Split(';');//adds line to list to array
+                int i = tab.Count();
+                for(int l=0; l < i; l++) { 
+                Console.Write(tab[l] + " ");
+                }
+                Console.WriteLine();
+                line = sr.ReadLine();//Goes to next line
+            }
+
+            sr.Close();//closes file
+            Console.ReadKey();
+
+
+       } 
+    }
+}
